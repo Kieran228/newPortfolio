f5a5a3240729a95e2d82c3ef966de4a3fba6bbec
Kieran Caraway <kierancaraway20@yahoo.com>
2025-02-15 23:48:39 -0600

    Navbar component creation
    redirected links to the proper path, imported a few assets from the assets folder. Also, set our initial states, interpolated styles from the styles folder with classname properties from tailwind. set the state in the onclick arrow function to empty and to scroll to the top of the page (0,0). imported the logo and changed the title, logo the title bar. made a few changes to the navigation panel

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles';
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
+import { li, link, nav } from 'framer-motion/client';
+
+const Navbar = () => {
+  const [active, setActive] = useState(' ')
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+        <Link
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Kieran <span className='sm:block hidden'>| Jr Developer</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li>
+              <a href={`${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
