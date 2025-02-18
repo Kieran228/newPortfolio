0a6a4fb40b060716ce039605ac06a71ec587328b
Kieran Caraway <kierancaraway20@yahoo.com>
2025-02-17 23:02:27 -0600

    latest update
    having issues with my navbar component... implemented a few solutions and didn't make any progress. Won't move on until issue with navbar is resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom'
 import { styles } from '../styles';
 import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
-import { li, link, nav } from 'framer-motion/client';
 
 const Navbar = () => {
-  const [active, setActive] = useState(' ')
+  const [active, setActive] = useState('');
 
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to="/"
           className='flex items-center gap-2'
@@ -27,8 +26,16 @@ const Navbar = () => {
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
-            <li>
-              <a href={`${link.id}`}>{link.title}</a>
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            > 
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
