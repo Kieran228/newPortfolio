d9083154d06da84861da3676b7c5ee8b8519086f
Kieran Caraway <154688088+Kieran228@users.noreply.github.com>
2025-03-10 13:09:36 -0500

    lighting adjustments. loader
    fixed some lighting properties to brighten up the 3d model while adding a percentage loader to indicate whether or not the model is loaded.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,8 @@
-import { Suspense, useEffect, useState } from "react"
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader'
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
 const computer = useGLTF('./desktop_pc/scene.gltf')
@@ -13,6 +13,14 @@ const computer = useGLTF('./desktop_pc/scene.gltf')
       <hemisphereLight intensity={0.15}
        groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight 
+        position={[ -20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive 
         object={computer.scene}
         scale={0.75}
@@ -26,13 +34,13 @@ const computer = useGLTF('./desktop_pc/scene.gltf')
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameloop="always"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
+        <OrbitControls 
           enableZoom={false}
           maxPolarAngle={Math.PI / 2} //? gives us rotation parameters 
           minPolarAngle={Math.PI / 2}
