5f6cd662307783f4cdce5c1b8c6fd0b1a8374094
Kieran Caraway <154688088+Kieran228@users.noreply.github.com>
2025-03-10 13:22:10 -0500

    scroller
    added a scroller indicator that will later redirect me to the about section. was made with framer motion and the properties were set to an infinite loop

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-        
+
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Kieran</span></h1>
           <p className={`${styles.heroSubText} m-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden" /> interfaces, and web applications
@@ -19,7 +19,27 @@ const Hero = () => {
         </div>
       </div>
 
-        <ComputersCanvas />
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 ">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+
+
     </section>
   )
 }
