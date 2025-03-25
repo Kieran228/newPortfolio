37334f83b01774131454a39417485c4166350a47
Kieran Caraway <154688088+Kieran228@users.noreply.github.com>
2025-03-25 18:22:27 -0500

    contact completion. project completion
    integrated the email functionality to where I can receive emails.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +18,48 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+// J9Z4DFlKmZjVK03y5
+// template_nkrpstb
+// service_8xrgxav
+
+    emailjs.send(
+      'service_8xrgxav',
+      'template_nkrpstb',
+      {
+        from_name: form.name,
+        to_name: 'Kieran',
+        from_email: form.email,
+        to_email: 'kierancaraway@gmail.com',
+        message: form.message
+      },
+      'J9Z4DFlKmZjVK03y5'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you! I will get back to you as soon as possible');
+
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+      alert("Something went wrong")
+    })
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
